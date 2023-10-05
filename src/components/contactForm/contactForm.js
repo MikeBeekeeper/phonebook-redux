162a9525cca3deae3a607bf0bdfd061f4709c72a
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-10-05 14:45:37 +0300

    fix package

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -57,9 +57,9 @@ const ContactForm = () => {
         id={nameInputId}
         value={name}
         onChange={handleChange}
-        // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        // required
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
       />
 
       <label htmlFor={numberInputId}> Number</label>
@@ -70,9 +70,9 @@ const ContactForm = () => {
         id={numberInputId}
         value={number}
         onChange={handleChange}
-        // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        // required
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
       />
       <button type="submit" className={css.submitBtn}>
         Add contact
