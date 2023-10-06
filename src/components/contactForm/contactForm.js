189b966994a47926a4fdc68900c41cfe67dd14ed
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-10-06 09:11:22 +0300

    without pattern

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -57,7 +57,6 @@ const ContactForm = () => {
         id={nameInputId}
         value={name}
         onChange={handleChange}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
       />
@@ -70,7 +69,6 @@ const ContactForm = () => {
         id={numberInputId}
         value={number}
         onChange={handleChange}
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
